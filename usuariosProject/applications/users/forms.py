799bb7424cbac2bde336f12b91aca841aa9fbca9
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-06-28 12:11:45 +0200

    Upgrade register with validations

--- usuariosProject/applications/users/forms.py
+++ usuariosProject/applications/users/forms.py
@@ -1,5 +1,6 @@
 # DJango imports
 from django import forms
+from django.contrib.admin.sites import site
 
 # Local models
 from .models import User
@@ -17,7 +18,7 @@ class UserRegisterForm(forms.ModelForm):
         )
     )
 
-    password_repeat = forms.CharField(
+    passwordRepeat = forms.CharField(
         label='Contraseña',
         required=True,
         widget=forms.PasswordInput(
@@ -38,3 +39,7 @@ class UserRegisterForm(forms.ModelForm):
             'last_name',
             'gender',
         )
+    
+    def clean_passwordRepeat(self):
+        if self.cleaned_data['password'] != self.cleaned_data['passwordRepeat'] and len(self.cleaned_data['password']) > 5 :
+            self.add_error('passwordRepeat', 'Las contraseñas son distintas o la longitud debe superar 5!')
