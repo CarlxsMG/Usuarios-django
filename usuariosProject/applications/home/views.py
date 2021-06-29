440d2a84c6ed4156fcd4a2c5189487ec098a7f5d
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-06-29 10:32:00 +0200

    Insert to home-panel LoginRequiredMixin

--- usuariosProject/applications/home/views.py
+++ usuariosProject/applications/home/views.py
@@ -4,6 +4,8 @@ import datetime
 
 # DJango imports
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 from django.views.generic import (
     TemplateView,
@@ -21,8 +23,9 @@ class FechaMixin(object):
         return context
 
         
-class HomePage(FechaMixin, TemplateView):
+class HomePage(LoginRequiredMixin, TemplateView):
     template_name = 'home/index.html'
+    login_url = reverse_lazy('users_app:user-login')
     
 
 class TemplateMixin(FechaMixin, TemplateView):
